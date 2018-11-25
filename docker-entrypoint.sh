10ae103a09e082795f6b5ec567751142c4be7ba9
Dr.Snowbird <DrSnowbird@openkbs.org>
2018-11-25 01:22:09 -0500

    update Dockerfile and entrypoint script

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,22 +2,22 @@
 
 set -e
 set -i
+set -x
 
 export JAVA_HOME=${JAVA_HOME:-"/usr/java"}
 export JAVACMD=${JAVA_HOME}/bin/java
 export EXE_COMMAND=${EXE_COMMAND:-"echo Hello"}
-
-echo "Starting docker process daemon ..."
-#### ------------------------------------------------------------------------
-#### ---- You need to set PRODUCT_EXE as the full-path executable binary ----
-#### ------------------------------------------------------------------------
-sudo -E /bin/bash -c "(JAVA_HOME=${JAVA_HOME:-/usr/java}; JAVACMD=${JAVA_HOME}/bin/java; ${EXE_COMMAND:-echo Hello})"
 
 #### ------------------------------------------------------------------------
 #### ---- Extra line added in the script to run all command line arguments
 #### ---- To keep the docker process staying alive if needed.
 #### ------------------------------------------------------------------------
 if [ $# -lt 1 ]; then
+    echo "Starting docker process daemon ..."
+    #### ------------------------------------------------------------------------
+    #### ---- You need to set PRODUCT_EXE as the full-path executable binary ----
+    #### ------------------------------------------------------------------------
+    sudo -E /bin/bash -c "(JAVA_HOME=${JAVA_HOME:-/usr/java}; JAVACMD=${JAVA_HOME}/bin/java; ${EXE_COMMAND:-echo Hello})"
     exec "/bin/bash";
 else
     exec "$@";
